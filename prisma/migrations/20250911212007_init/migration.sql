a759c6fd9a30057c0137dd2863c76c711e1d7a44
Will Pate <willpate6500@protonmail.com>
2025-09-11 17:26:12 -0400

    updates

---
+++ prisma/migrations/20250911212007_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."Submission" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Submission_pkey" PRIMARY KEY ("id")
+);
