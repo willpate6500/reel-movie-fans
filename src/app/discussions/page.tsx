02765ae42f6e7ca6040f3cad02b91ecc7e33cc23
Will Pate <willpate6500@protonmail.com>
2025-09-12 12:12:14 -0400

    escape '

--- src/app/discussions/page.tsx
+++ src/app/discussions/page.tsx
@@ -5,10 +5,10 @@ export default function Discussions() {
         <h1>ReelMovieFans Watch Party Discussions</h1>
         <p>To prepare for watch parties:</p>
         <ul>
-          <li>Watch the week's film beforehand.</li>
+          <li>Watch the week&#39;s film beforehand.</li>
           <li>Preferably, watch it again and take notes.</li>
           <li>Do some background research into the production and public response.</li>
-          <li>Sign up for the week's discussion, and we'll share a zoom link!</li>
+          <li>Sign up for the week&#39;s discussion, and we&#39;ll share a zoom link!</li>
         </ul>
 
         <p className="bg-black text-black">
